6d1091d744aa1e54416ebb0b941896cb15592f0c
Carine Morel <carine@tarides.com>
2026-05-27 19:10:19 +0200

    clean up

--- src/motyper.ml
+++ src/motyper.ml
@@ -6,42 +6,26 @@ type parsedtree = (string * Moparser.expr) list
 type typedtree = (string * int) list ref
 type result = { config : Moconfig.t; typedtree : typedtree }
 
-exception Cancel_or_closing
-exception Exception_after_partial of exn
-
-type msg =
-  | Msg of [ `Close | `Cancel | `Exn of exn ]
-  | Config of Moconfig.t
-  | Partial of result
-
 (* In mtyper.ml, type_structure, type_implementation and run returns different types*)
 type env = (string * int) list
 
 let res : typedtree = ref []
 
-let type_structure env parsedtree =
-  let rec loop : env -> parsedtree -> typedtree =
-   fun env ldefs ->
+let type_structure (env : env) parsedtree =
+  let rec loop env count parsedtree : typedtree =
     (* Utils.log 1 "Typing defs %d / %d" count (List.length parsedtree); *)
-    let typer_state =
-      match ldefs with
-      | def :: rest ->
-          begin try
-            let v, e = Moparser.eval_item env def in
-            res := (v, e) :: !res;
-            `Rest (rest, (v, e))
-          with exn -> `Exn exn
-          end
-      | [] -> `Finish
-    in
-    match typer_state with
-    | `Exn exn -> raise exn
-    | `Finish -> res
-    | `Rest (rest, (v, e)) ->
-        Utils.stupid_work ();
-        loop ((v, e) :: env) rest
+    match parsedtree with
+    | def :: rparsedtree ->
+        begin try
+          let v, e = Moparser.eval_item env def in
+          res := (v, e) :: !res;
+          Utils.stupid_work ();
+          loop ((v, e) :: env) (count + 1) rparsedtree
+        with exn -> raise exn
+        end
+    | [] -> res
   in
-  loop env parsedtree
+  loop env 0 parsedtree
 
 let type_implementation config parsedtree =
   match config.completion with
--- src/motyper.mli
+++ src/motyper.mli
@@ -2,12 +2,4 @@ type parsedtree = (string * Moparser.expr) list
 type typedtree = (string * int) list ref
 type result = { config : Moconfig.t; typedtree : typedtree }
 
-exception Cancel_or_closing
-exception Exception_after_partial of exn
-
-type msg =
-  | Msg of [ `Cancel | `Close | `Exn of exn ]
-  | Config of Moconfig.t
-  | Partial of result
-
 val run : Moconfig.t -> parsedtree -> result
